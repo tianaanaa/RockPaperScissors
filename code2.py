7cf040c266481a3cf7a328316a9ef5c9cd870849
tianaanaa <tmurph55@students.kennesaw.edu>
2021-11-24 18:10:40 -0500

    Update code2.py

---
+++ code2.py
@@ -0,0 +1,30 @@
+import random
+# allow program to take user input
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+# results of the game varies by the rules of rock, paper, scissors
+def determine_winner(user_action, computer_action):
+    if user_action == computer_action:
+        print(f"Both players selected {user_action.name}. It's a tie!")
+    elif user_action == Action.Rock:
+        if computer_action == Action.Scissors:
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == Action.Paper:
+        if computer_action == Action.Rock:
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == Action.Scissors:
+        if computer_action == Action.Paper:
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+# game ends when user selects if they want not play again
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
